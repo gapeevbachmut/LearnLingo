217dd9323da811a62753961dc235e76eda0771ea
gapeevbachmut <gapeev1979@gmail.com>
2025-12-21 16:18:04 +0000

    Deploying to gh-pages from @ gapeevbachmut/LearnLingo@d1136d8eadf3b18a79175f6d8fb9f3021619a2cc 🚀

--- assets/main-WOCeEEy0.js
+++ assets/main-WOCeEEy0.js
@@ -85,4 +85,4 @@ ${e.levels.map(a=>`<li class='teacher-level'><p>#${a}</p></li>`).join("")}
   </div>
 </li>
     `}const h="https://learnlingo-12-25-default-rtdb.firebaseio.com/.json";async function g(){const e=await d.get(h);return e.data?Object.values(e.data):[]}const f=document.querySelector(".teachers-list"),i=document.querySelector(".load-more-btn"),v=4;let l=1,c=[];async function m(){c=await g(),p()}function p(){const e=l*v,a=c.slice(0,e),r=a.map(n=>u(n)).join("");f.innerHTML=r,a.length>=c.length&&(i.style.display="none")}i.addEventListener("click",()=>{l+=1,p()});m();const y=new Date;document.querySelector(".footer-date");const b=document.querySelector(".date-year");b.textContent=y.getFullYear();
-//# sourceMappingURL=main-DAeilqzI.js.map
+//# sourceMappingURL=main-WOCeEEy0.js.map
