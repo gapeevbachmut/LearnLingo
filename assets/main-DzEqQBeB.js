1827b6ffa2dbcc2261f2d0afc8a43f83df095ccb
gapeevbachmut <gapeev1979@gmail.com>
2025-12-22 19:37:23 +0000

    Deploying to gh-pages from @ gapeevbachmut/LearnLingo@ba9e60dd82fa64af896ed1cbf95dcf85a1819407 🚀

---
+++ assets/main-DzEqQBeB.js
@@ -0,0 +1,122 @@
+import{a as y}from"./vendor-BWC8OeqA.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&s(c)}).observe(document,{childList:!0,subtree:!0});function a(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(t){if(t.ep)return;t.ep=!0;const o=a(t);fetch(t.href,o)}})();const b=`<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" style="position:absolute;width:0;height:0;overflow:hidden">
+  <defs>
+    <symbol id="icon-log-in" viewBox="0 0 32 32">
+      <path fill="none" stroke="#f4c550" stroke-linecap="round" stroke-linejoin="round" stroke-width="3.2" d="M20 4h1.6c2.24 0 3.36 0 4.216.436a3.996 3.996 0 0 1 1.748 1.748C28 7.04 28 8.16 28 10.4v11.2c0 2.24 0 3.36-.436 4.216a4.001 4.001 0 0 1-1.748 1.748C24.96 28 23.84 28 21.6 28H20M13.333 9.333 20 16m0 0-6.667 6.667M20 16H4" style="stroke:var(--color1, #f4c550)"/>
+    </symbol>
+    <symbol id="icon-star" viewBox="0 0 32 32">
+      <path fill="#ffc531" stroke="#ffc531" stroke-width="2.4" d="M19.6 9.395a2.201 2.201 0 0 0 1.57 1.141l7.6 1.313-5.375 5.535a2.2 2.2 0 0 0-.6 1.846l1.098 7.635-6.922-3.402-.234-.1a2.203 2.203 0 0 0-1.473 0l-.234.1-6.924 3.402 1.1-7.635a2.2 2.2 0 0 0-.6-1.846l-5.377-5.535 7.602-1.313a2.198 2.198 0 0 0 1.57-1.141l3.6-6.822 3.6 6.822z" style="fill:var(--color2, #ffc531);stroke:var(--color2, #ffc531)"/>
+    </symbol>
+    <symbol id="icon-ukraine" viewBox="0 0 32 32">
+      <path fill="#ffda44" d="M16 32c8.837 0 16-7.163 16-16S24.837 0 16 0 0 7.163 0 16s7.163 16 16 16z" style="fill:var(--color3, #ffda44)"/>
+      <path fill="#338af3" d="M0 16C0 7.164 7.163 0 16 0s16 7.163 16 16z" style="fill:var(--color4, #338af3)"/>
+    </symbol>
+    <symbol id="icon-book-open" viewBox="0 0 32 32">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M29.333 8.267c0-1.493 0-2.24-.291-2.811a2.666 2.666 0 0 0-1.165-1.165C27.306 4 26.56 4 25.066 4h-.533c-2.987 0-4.48 0-5.621.581a5.331 5.331 0 0 0-2.331 2.331C16 8.053 16 9.546 16 12.533V28l.133-.2c.926-1.389 1.389-2.084 2.001-2.587a5.345 5.345 0 0 1 1.837-.983c.758-.23 1.593-.23 3.262-.23h1.833c1.494 0 2.24 0 2.811-.291.502-.256.91-.664 1.165-1.165.291-.57.291-1.317.291-2.811V8.266zM2.667 8.267c0-1.493 0-2.24.291-2.811.256-.502.664-.91 1.165-1.165C4.693 4 5.44 4 6.934 4h.533c2.987 0 4.48 0 5.621.581a5.331 5.331 0 0 1 2.331 2.331C16 8.053 16 9.546 16 12.533V28l-.133-.2c-.926-1.389-1.389-2.084-2.001-2.587a5.345 5.345 0 0 0-1.837-.983C11.271 24 10.436 24 8.767 24H6.934c-1.493 0-2.24 0-2.811-.291a2.666 2.666 0 0 1-1.165-1.165c-.291-.57-.291-1.317-.291-2.811V8.266z"/>
+    </symbol>
+    <symbol id="icon-hover" viewBox="0 0 32 32">
+      <path fill="#f4c550" stroke="#f4c550" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.231" d="M27.787 6.147a7.326 7.326 0 0 0-10.373 0L16.001 7.56l-1.413-1.413a7.333 7.333 0 0 0-10.374 0 7.333 7.333 0 0 0 0 10.374l11.787 11.787 11.787-11.787a7.326 7.326 0 0 0 0-10.373z" style="fill:var(--color1, #f4c550);stroke:var(--color1, #f4c550)"/>
+    </symbol>
+    <symbol id="icon-normal" viewBox="0 0 32 32">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.462" d="M27.787 6.147a7.326 7.326 0 0 0-10.373 0L16.001 7.56l-1.413-1.413a7.333 7.333 0 0 0-10.374 0 7.333 7.333 0 0 0 0 10.374l11.787 11.787 11.787-11.787a7.326 7.326 0 0 0 0-10.373z"/>
+    </symbol>
+    <symbol id="icon-group" viewBox="0 0 32 32">
+      <path fill="#fff" d="M32 16c0 8.837-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0s16 7.163 16 16z" style="fill:var(--color5, #fff)"/>
+      <path fill="#38cd3e" d="M26.667 16c0 5.891-4.776 10.667-10.667 10.667S5.333 21.891 5.333 16 10.109 5.333 16 5.333 26.667 10.109 26.667 16z" style="fill:var(--color6, #38cd3e)"/>
+    </symbol>
+    <symbol id="icon-eye-off" viewBox="0 0 24 24">
+      <path d="M10.128 5.214A7.908 7.908 0 0 1 11.988 5c1.758 0 3.309.559 4.658 1.393 1.119.692 2.089 1.567 2.894 2.448a18.701 18.701 0 0 1 2.318 3.156 16.723 16.723 0 0 1-1.778 2.542 1 1 0 0 0 1.518 1.302 18.991 18.991 0 0 0 2.283-3.37 1 1 0 0 0 .013-.919s-.396-.789-1.12-1.843a20.553 20.553 0 0 0-1.757-2.218c-.894-.979-2.004-1.987-3.319-2.8C16.103 3.706 14.192 3 12.012 3a9.899 9.899 0 0 0-2.34.266 1 1 0 1 0 .456 1.948zm-.101 6.228 2.531 2.531a2.003 2.003 0 0 1-2.531-2.532zM5.983 7.397 8.536 9.95a3.993 3.993 0 0 0-.608 2.266c.036 1.022.463 2.033 1.271 2.785s1.846 1.107 2.868 1.071a3.991 3.991 0 0 0 1.984-.608l2.322 2.322A8.962 8.962 0 0 1 11.989 19c-1.734 0-3.285-.559-4.634-1.393-1.119-.692-2.089-1.567-2.894-2.448a18.701 18.701 0 0 1-2.317-3.155 17.275 17.275 0 0 1 3.841-4.607zm-5.69-5.69 4.271 4.271a19.34 19.34 0 0 0-4.445 5.549.999.999 0 0 0-.013.92s.396.789 1.12 1.843a20.553 20.553 0 0 0 1.757 2.218c.894.979 2.004 1.987 3.319 2.8C7.897 20.294 9.808 21 12.012 21a10.98 10.98 0 0 0 5.815-1.759l4.466 4.466a.999.999 0 1 0 1.414-1.414l-8.876-8.876-.007-.007-4.209-4.21-.024-.024L1.707.293A.999.999 0 1 0 .293 1.707z"/>
+    </symbol>
+    <symbol id="icon-eye" viewBox="0 0 24 24">
+      <path d="M.106 11.553a.997.997 0 0 0 0 .894s.396.789 1.12 1.843a20.553 20.553 0 0 0 1.757 2.218c.894.979 2.004 1.987 3.319 2.8C7.897 20.294 9.808 21 12 21s4.103-.706 5.698-1.692c1.315-.813 2.425-1.821 3.319-2.8a20.678 20.678 0 0 0 1.757-2.218 17.468 17.468 0 0 0 1.12-1.843.997.997 0 0 0 0-.894s-.396-.789-1.12-1.843a20.553 20.553 0 0 0-1.757-2.218c-.894-.979-2.004-1.987-3.319-2.8C16.103 3.706 14.192 3 12 3s-4.103.706-5.698 1.692c-1.315.813-2.425 1.821-3.319 2.8A20.455 20.455 0 0 0 1.226 9.71a17.468 17.468 0 0 0-1.12 1.843zM2.14 12a18.575 18.575 0 0 1 2.319-3.159c.805-.881 1.775-1.756 2.894-2.448C8.703 5.559 10.254 5 12 5s3.297.559 4.646 1.393c1.119.692 2.089 1.567 2.894 2.448A18.701 18.701 0 0 1 21.859 12a18.575 18.575 0 0 1-2.319 3.159c-.805.881-1.775 1.756-2.894 2.448C15.297 18.441 13.746 19 12 19s-3.297-.559-4.646-1.393c-1.119-.692-2.089-1.567-2.894-2.448A18.701 18.701 0 0 1 2.14 12zM16 12c0-1.104-.449-2.106-1.172-2.828a3.994 3.994 0 0 0-5.656 0 3.994 3.994 0 0 0 0 5.656 3.994 3.994 0 0 0 5.656 0A3.994 3.994 0 0 0 16 12zm-2 0c0 .553-.223 1.051-.586 1.414S12.553 14 12 14s-1.051-.223-1.414-.586S10 12.553 10 12s.223-1.051.586-1.414S11.447 10 12 10s1.051.223 1.414.586S14 11.447 14 12z"/>
+    </symbol>
+  </defs>
+</svg>
+`;function w(){const e=document.createElement("svg");e.style.display="none",e.innerHTML=b,document.body.prepend(e)}function L(e){return`
+<li class="teacher-card">
+  <!-- Avatar -->
+  <div class="teacher-avatar">
+    <img src='${e.avatar_url||"../img/main-content/default-avatar.png"} '
+     alt="${e.name||"Teacher"} " class="teacher-avatar-img" width="96"
+      height="96" />
+    <svg width="12" height="12" class="teacher-avatar-svg">
+      <use href="#icon-group"></use>
+    </svg>
+  </div>
+
+  <!-- Main content -->
+  <div class="teacher-content">
+    <div class="teacher-content-top">
+      <!-- Name -->
+      <div class="teacher-data">
+                <p class="teacher-languages">${e.languages}</p>
+
+        <h2 class="teacher-name">
+          ${e.name} ${e.surname}
+        </h2>
+      </div>
+      <!-- Top info row -->
+      <div class="teacher-meta">
+
+        <p class="teacher-lessons-online">
+                    <svg width="16" height="16" class="svg-book">
+            <use href="#icon-book-open"></use>
+          </svg>
+          Lessons online
+        </p>
+        <span class="teacher-separator">|</span>
+        <p class="teacher-lessons-done">
+          Lessons done: <span>${e.lessons_done}</span>
+        </p>
+        <span class="teacher-separator">|</span>
+        <p class="teacher-rating">
+          <svg width="16" height="16" class="svg-star">
+            <use href="#icon-star"></use>
+          </svg>
+          Rating: <span>${e.rating}</span>
+        </p>
+        <span class="teacher-separator">|</span>
+        <p class="teacher-price">
+          Price / 1 hour: <span class="price-color">${e.price_per_hour}$</span>
+        </p>
+        <button class="teacher-like-btn" type="button">
+          <svg width="16" height="16" class="svg-heart">
+            <use href="#icon-normal"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <!-- Skills / description -->
+    <div class="teacher-info">
+
+      <p class="teacher-speaks">
+        <span class="teacher-info-label">Speaks:</span>
+        <span class="teacher-info-language"> ${e.languages}</span>
+
+      </p>
+
+      <p class="teacher-lesson-info">
+        <span class="teacher-info-label">Lesson Info:</span>
+${e.lesson_info}      </p>
+
+      <p class="teacher-conditions">
+        <span class="teacher-info-label">Conditions:</span>
+${e.conditions}      </p>
+
+    </div>
+
+    <!-- Read more -->
+    <button class="teacher-read-more" type="button">
+      Read more
+    </button>
+
+    <!-- Levels -->
+    <ul class="teacher-levels">
+${e.levels.map(n=>`<li class='teacher-level'><p>#${n}</p></li>`).join("")}
+
+    </ul>
+
+  </div>
+</li>
+    `}const k="https://learnlingo-12-25-default-rtdb.firebaseio.com/.json";async function M(){const e=await y.get(k);return e.data?Object.values(e.data):[]}const S=document.querySelector(".teachers-list"),g=document.querySelector(".load-more-btn"),C=4;let v=1,l=[];function r(e){l=e,v=1,m()}function m(){const e=v*C,n=l.slice(0,e),a=n.map(s=>L(s)).join("");S.innerHTML=a,n.length>=l.length&&(g.style.display="none")}g.addEventListener("click",()=>{v+=1,m()});const i=document.querySelector(".language");let d=[];function $(e){d=e;const n=e.flatMap(s=>s.languages);[...new Set(n)].forEach(s=>{const t=document.createElement("option");t.value=s,t.textContent=s,i.append(t)}),i.value=""}i.addEventListener("change",e=>{const n=e.target.value;if(!n){r(d);return}const a=d.filter(s=>s.languages.includes(n));r(a)});const z=new Date;document.querySelector(".footer-date");const T=document.querySelector(".date-year");T.textContent=z.getFullYear();const u=document.querySelector(".level");let p=[];function x(e){p=e;const n=e.flatMap(s=>s.levels);[...new Set(n)].forEach(s=>{const t=document.createElement("option");t.value=s,t.textContent=s,u.append(t)}),u.value=""}u.addEventListener("change",e=>{const n=e.target.value;if(!n){r(p);return}const a=p.filter(s=>s.levels.includes(n));r(a)});const f=document.querySelector(".price");let h=[];function q(e){h=e;const n=e.map(s=>s.price_per_hour);[...new Set(n)].sort((s,t)=>s-t).forEach(s=>{const t=document.createElement("option");t.value=s,t.textContent=`${s} $`,f.append(t)}),f.value=""}f.addEventListener("change",e=>{const n=e.target.value,a=Number(n);if(!n){r(h);return}const s=h.filter(t=>t.price_per_hour===a);r(s)});w();async function E(){const e=await M();r(e),$(e),x(e),q(e)}E();
+//# sourceMappingURL=main-DzEqQBeB.js.map
