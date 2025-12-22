7bd60711dd47d592b74f3560992e2fbd863b4426
gapeevbachmut <gapeev1979@gmail.com>
2025-12-22 16:28:37 +0000

    Deploying to gh-pages from @ gapeevbachmut/LearnLingo@783232ddaa8a12cf2521b09c72d97370c8c783b3 🚀

--- assets/main-qWDZSSNu.js
+++ assets/main-qWDZSSNu.js
@@ -5,7 +5,7 @@ import{a as L}from"./vendor-BWC8OeqA.js";(function(){const n=document.createElem
     <img src='${e.avatar_url}' alt="${e.name}" class="teacher-avatar-img" width="96"
       height="96" />
     <svg width="12" height="12" class="teacher-avatar-svg">
-      <use href="/img/sprite.svg#icon-group"></use>
+      <use href="/img/sprite-2.svg#icon-group"></use>
     </svg>
   </div>
 
@@ -25,7 +25,7 @@ import{a as L}from"./vendor-BWC8OeqA.js";(function(){const n=document.createElem
 
         <p class="teacher-lessons-online">
                     <svg width="16" height="16" class="svg-book">
-            <use href="/img/book-open.svg"></use>
+            <use href="/img/sprite-2.svg#icon-book-open"></use>
           </svg>
           Lessons online
         </p>
@@ -36,7 +36,7 @@ import{a as L}from"./vendor-BWC8OeqA.js";(function(){const n=document.createElem
         <span class="teacher-separator">|</span>
         <p class="teacher-rating">
           <svg width="16" height="16" class="svg-star">
-            <use href="/img/sprite.svg#icon-star"></use>
+            <use href="/img/sprite-2.svg#icon-star"></use>
           </svg>
           Rating: <span>${e.rating}</span>
         </p>
@@ -46,7 +46,7 @@ import{a as L}from"./vendor-BWC8OeqA.js";(function(){const n=document.createElem
         </p>
         <button class="teacher-like-btn" type="button">
           <svg width="16" height="16" class="svg-heart">
-            <use href="/img/sprite.svg#icon-normal"></use>
+            <use href="/img/sprite-2.svg#icon-normal"></use>
           </svg>
         </button>
       </div>
@@ -85,4 +85,4 @@ ${e.levels.map(n=>`<li class='teacher-level'><p>#${n}</p></li>`).join("")}
   </div>
 </li>
     `}const b="https://learnlingo-12-25-default-rtdb.firebaseio.com/.json";async function $(){const e=await L.get(b);return e.data?Object.values(e.data):[]}const q=document.querySelector(".teachers-list"),v=document.querySelector(".load-more-btn"),S=4;let f=1,l=[];function c(e){l=e,f=1,m()}function m(){const e=f*S,n=l.slice(0,e),a=n.map(s=>y(s)).join("");q.innerHTML=a,n.length>=l.length&&(v.style.display="none")}v.addEventListener("click",()=>{f+=1,m()});const i=document.querySelector(".language");let u=[];function T(e){u=e;const n=e.flatMap(s=>s.languages);[...new Set(n)].forEach(s=>{const t=document.createElement("option");t.value=s,t.textContent=s,i.append(t)}),i.value=""}i.addEventListener("change",e=>{const n=e.target.value;if(!n){c(u);return}const a=u.filter(s=>s.languages.includes(n));c(a)});const w=new Date;document.querySelector(".footer-date");const E=document.querySelector(".date-year");E.textContent=w.getFullYear();const p=document.querySelector(".level");let d=[];function P(e){d=e;const n=e.flatMap(s=>s.levels);[...new Set(n)].forEach(s=>{const t=document.createElement("option");t.value=s,t.textContent=s,p.append(t)}),p.value=""}p.addEventListener("change",e=>{const n=e.target.value;if(!n){c(d);return}const a=d.filter(s=>s.levels.includes(n));c(a)});const g=document.querySelector(".price");let h=[];function _(e){h=e;const n=e.map(s=>s.price_per_hour);[...new Set(n)].sort((s,t)=>s-t).forEach(s=>{const t=document.createElement("option");t.value=s,t.textContent=`${s} $`,g.append(t)}),g.value=""}g.addEventListener("change",e=>{const n=e.target.value,a=Number(n);if(!n){c(h);return}const s=h.filter(t=>t.price_per_hour===a);c(s)});async function C(){const e=await $();c(e),T(e),P(e),_(e)}C();
-//# sourceMappingURL=main-DmOH1CjO.js.map
+//# sourceMappingURL=main-qWDZSSNu.js.map
