83adea933c861c0770258372c7e1ef9c88f98bae
gapeevbachmut <gapeev1979@gmail.com>
2026-01-06 07:52:28 +0000

    Deploying to gh-pages from @ gapeevbachmut/LearnLingo@b8bbb88cdb836b2b66b465ba051f269035cfef43 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{i}from"./assets/footer-3rjEJK3v.js";import"./assets/vendor-D2CNYbfS.js";i();
+import{i}from"./assets/footer-Caa3Bft0.js";import"./assets/vendor-D2CNYbfS.js";i();
 //# sourceMappingURL=index.js.map
