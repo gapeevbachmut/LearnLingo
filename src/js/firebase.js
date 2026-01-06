a50a3efa7e9c8c6bd661824f109bd7cde1915832
Андрій <gapeev1979@gmail.com>
2026-01-06 10:52:45 +0200

    fix privat and change key

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -4,15 +4,16 @@ import { initializeApp } from 'firebase/app';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth } from 'firebase/auth';
+import 'dotenv/config';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: '***',
-  authDomain: '***',
-  projectId: '***',
-  storageBucket: '***',
-  messagingSenderId: '***',
-  appId: '***',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUSKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
